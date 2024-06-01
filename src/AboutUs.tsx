b83c0c1f71ca0134c2e9924ede6a06b86f4f7dd4
P.Bharath Kumar <bharathkumar0603@gmail.com>
2024-06-01 15:39:48 +0530

    navbar misc in about page

--- src/AboutUs.tsx
+++ src/AboutUs.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import './AboutUs.css';  // Import the CSS file
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons'; 
+import Logo1 from './assets/logo.png';
+import { Link } from 'react-router-dom';
+
 
 const AboutUs: React.FC = () => {
   return (
+    <div className='abt-cont'>
+      <div className="navbar">
+        <div className="logo">
+          <img src={Logo1} alt="Logo1" />
+        </div>
+        <ul>
+          <li><Link to="/Ustaad/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          {/* <li><a href="#placements">Placements</a></li>
+          <li><a href="#facilities">Facilities</a></li> */}
+          <li><a href="/contact">Contact Us</a></li>
+          <li><a href="/user"><FontAwesomeIcon icon={faUser} /></a></li>
+        </ul>
+      </div>
     <div className="about-us-container">
       <div className="about-us-box">
         <h1 className="about-us-title">About <span>Ustaad</span></h1>
@@ -16,6 +35,7 @@ const AboutUs: React.FC = () => {
           At Ustaad, we believe in the power of education and mentorship to transform lives and shape the future.
         </p>
       </div>
+    </div>
     </div>
   );
 };
